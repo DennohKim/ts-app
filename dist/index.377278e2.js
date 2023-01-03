ca6f567b74ec29e403569c7989372be08603b8e4
“dennohkim” <kimathidennis38@gmail.com>
2023-01-03 20:44:53 +0300

    boolean type

--- dist/index.377278e2.js
+++ dist/index.377278e2.js
@@ -1,8 +1,2 @@
-function multiplyByTwo(number) {
-    if (typeof number === "number") return number * 2;
-    return "Please provide a valid number";
-}
-console.log(multiplyByTwo(4));
-console.log(multiplyByTwo("John"));
 
 //# sourceMappingURL=index.377278e2.js.map
--- src/index.ts
+++
@@ -1,9 +0,0 @@
-function multiplyByTwo(number: unknown){
-    if (typeof number === "number") {
-        return number * 2
-    }
-    return "Please provide a valid number";
-}
-
-console.log(multiplyByTwo(4))
-console.log(multiplyByTwo("John"))
