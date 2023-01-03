9e7431835b949c6a336701c6fb22b24ab6f538ac
“dennohkim” <kimathidennis38@gmail.com>
2023-01-03 20:37:10 +0300

    unkown type

--- dist/index.377278e2.js
+++ dist/index.377278e2.js
@@ -1,15 +1,8 @@
-//Any type
-//Avoid any type. This is default javascript behavior
-let firstName = "Mark";
-firstName = 123;
-firstName = [
-    5,
-    4,
-    8,
-    4
-];
 function multiplyByTwo(number) {
-    return number * 2;
+    if (typeof number === "number") return number * 2;
+    return "Please provide a valid number";
 }
+console.log(multiplyByTwo(4));
+console.log(multiplyByTwo("John"));
 
 //# sourceMappingURL=index.377278e2.js.map
