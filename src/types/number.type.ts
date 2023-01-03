6c0642dc0aa6a5e3fa48f546926800e7453d477b
“dennohkim” <kimathidennis38@gmail.com>
2023-01-03 21:17:15 +0300

    number type

---
+++ src/types/number.type.ts
@@ -0,0 +1,17 @@
+let number: number = 100;
+
+//Floats are also numbers
+
+number = 128.3
+
+//Negative numbers are also types
+number = -200;
+
+//Expression that evaluates to a number
+
+number = Infinity *0.01
+
+number = parseInt('12');
+
+
+
