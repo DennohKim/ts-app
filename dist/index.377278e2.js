b6316a8ecba6000b09d3c8fab3be6b6ccd26e2e1
“dennohkim” <kimathidennis38@gmail.com>
2023-01-04 15:35:07 +0300

    union types

--- dist/index.377278e2.js
+++ dist/index.377278e2.js
@@ -1,18 +1,2 @@
-//Union type needs to have all of the properties of one of the types
-let dog = {
-    name: "Buddy",
-    barks: true,
-    wags: true
-};
-let cat = {
-    name: "Bella",
-    purrs: true
-};
-let dogAndCat = {
-    name: "Buddy",
-    barks: true,
-    wags: true,
-    purrs: false
-};
 
 //# sourceMappingURL=index.377278e2.js.map
---
+++ src/types/uniontypes.type.ts
@@ -0,0 +1,34 @@
+//Union type needs to have all of the properties of one of the types
+
+type Dog = {
+    name: string;
+    barks: boolean;
+    wags: boolean;
+  };
+  
+  type Cat = {
+    name: string;
+    purrs: boolean;
+  };
+  
+  //join the union using  a pipe
+  type DogAndCatUnion = Dog | Cat;
+  
+  let dog: DogAndCatUnion = {
+    name: "Buddy",
+    barks: true,
+    wags: true,
+  };
+  
+  let cat: DogAndCatUnion = {
+    name: "Bella",
+    purrs: true,
+  };
+  
+  let dogAndCat: DogAndCatUnion = {
+    name: "Buddy",
+    barks: true,
+    wags: true,
+    purrs: false,
+  };
+  
