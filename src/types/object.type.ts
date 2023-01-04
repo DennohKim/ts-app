c3fd178be7d2d1ab4f0a6894a6efeb5c116f1b85
“dennohkim” <kimathidennis38@gmail.com>
2023-01-04 14:55:56 +0300

    object types

---
+++ src/types/object.type.ts
@@ -0,0 +1,20 @@
+//With type inference no error is thrown
+let person = {
+    name: "chizaa",
+    age: 25,
+  };
+  
+  let car: { color: string; brand: string } = {
+      color: 'red',
+      brand: 'bmw',
+  };
+  
+  
+  let article: { author: string; content: string; article: string; image?: string} = {
+      author: "chizaa",
+      content: "web 3",
+      article: "solidity",
+  
+  }
+  
+  
