ad1c7c975831c90d925fc60fcf0252a3e6303161
“dennohkim” <kimathidennis38@gmail.com>
2023-01-04 14:32:57 +0300

    string type

--- dist/index.377278e2.js
+++ dist/index.377278e2.js
@@ -1,5 +1,2 @@
-let firstname = "Mark";
-let fullName = firstname + " " + "Doe";
-fullName.split(" "); //fullName = 123456;
 
 //# sourceMappingURL=index.377278e2.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,2 @@
-let firstname: string = "Mark";
-
-let fullName: string = firstname + " " + "Doe"
-
-fullName.split(" ");
-
-//fullName = 123456;
-
-
-
 
  
